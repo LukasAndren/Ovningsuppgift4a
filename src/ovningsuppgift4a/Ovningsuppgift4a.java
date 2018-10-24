d7d08715b008dd14ca44559ef1b709d19a0f7eb0
Lukas Andrén <jlukasaa@gmail.com>
2018-10-24 11:01:50 +0200

    Second commit

--- src/ovningsuppgift4a/Ovningsuppgift4a.java
+++ src/ovningsuppgift4a/Ovningsuppgift4a.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package ovningsuppgift4a;
 
@@ -19,7 +14,8 @@ import java.util.logging.Logger;
 import javax.swing.*;
 
 /**
- *
+ * testtest branch2test
+ * asdasdasdasdasdasdkj
  * @author Lukas test
  */
 public class Ovningsuppgift4a extends JFrame implements ActionListener{
